fc70d9b1523badfa4abc16e779356c1ff11939cc
jukdang <jukdang479@gmail.com>
2025-11-06 11:34:53 +0900

    factory method 추가

--- src/main/java/egovframework/theimc/common/model/ApiResponse.java
+++ src/main/java/egovframework/theimc/common/model/ApiResponse.java
@@ -26,4 +26,36 @@ public class ApiResponse<T> {
     this.data = null;
 	}	
 
+	public static <T> ApiResponse<T> success(String message) {
+		return ApiResponse.<T>builder()
+				.code(HttpStatus.OK.value())
+				.message(message)
+				.data(null)
+				.build();
+	}
+
+	public static <T> ApiResponse<T> success(String message, T data) {
+		return ApiResponse.<T>builder()
+				.code(HttpStatus.OK.value())
+				.message(message)
+				.data(data)
+				.build();
+	}
+
+	public static <T> ApiResponse<T> success(T data) {
+		return ApiResponse.<T>builder()
+				.code(HttpStatus.OK.value())
+				.message("요청이 성공적으로 처리되었습니다.")
+				.data(data)
+				.build();
+	}
+
+	public static <T> ApiResponse<T> error(HttpStatus status, String message) {
+		return ApiResponse.<T>builder()
+				.code(status.value())
+				.message(message)
+				.data(null)
+				.build();
+	}
+
 }
